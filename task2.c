390b6c1d9d262a407ee019844c57338a84182cac
WillAlley-official <walley@binghamton.edu>
2025-09-19 12:06:54 -0400

    ungwah pt2

--- task2.c
+++ task2.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
 #define INPUT_FILE "./input/if1"
 
 int main(int argc, char * argv[]) {
-    
+	pid_t pid = fork();
+
+	// Child Process
+	if(pid == 0) {
+
+		printf("IN CHILD: pid=%d\n", getpid());
+		int fd = open(INPUT_FILE);
+		dup2(fd, STDIN_FILENO);
+		int output = open("./result", O_CREAT);
+		dup2(output, STDOUT_FILENO);
+		execvp(argv[1], &argv[1]);
+
+	// Parent Process
+	} else {
+		
+		wait(0);
+		printf("IN PARENT: successfully waited child (pid=%d)\n", pid);
+
+	}	
     return 0;
 }
