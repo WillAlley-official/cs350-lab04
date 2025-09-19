a8cd5ed8f77209840506a950d994a2b5a580921b
WillAlley-official <walley@binghamton.edu>
2025-09-19 11:26:59 -0400

    ungwah part 1

--- task1.c
+++ task1.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main(int argc, char * argv[]) {
+
+	// Forks
+	pid_t pid = fork()
+
+	// Child Process
+	if(pid == 0) {
+
+		printf("IN CHILD: pid=%d\n", getpid());
+		execvp(argv[1], argv);
+
+	// Parent Process
+	} else {
+		
+		wait();
+		printf("IN PARENT: successfully waited child (pid=%d)\n", pid);
+	
+	}
     
     return 0;
 }
